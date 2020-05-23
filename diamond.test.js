5dbb38bf70127ad19e3e43d1e6e36d26ccabe3fe
yasminfunk <64017359+yasminfunk@users.noreply.github.com>
2020-05-23 11:44:55 +0200

    create diamond.test.js

---
+++ diamond.test.js
@@ -0,0 +1,19 @@
+const {diamond, edge} = require ('./src/diamond.js')
+
+describe ("diamond", () => {
+    it ("it should output A for A", () => {
+        expect (diamond ("A")).toEqual("A")
+    })
+    describe ("edge", () => {
+        it ("should return -A\nB- for B", () => {
+            expect (edge("B")).toEqual(["-A",
+                                         "B-"])
+        })
+        describe ("diamond", () => {
+            it ("it should output --A--\n-B-B-\nC---C\n-B-B-\n--A-- for C", () => {
+                expect (diamond ("C")).toEqual(["--A--" , "-B-B-", "C---C", "-B-B-", "--A--"])
+            })
+        })
+
+    })
+})
