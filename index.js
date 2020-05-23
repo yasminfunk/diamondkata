77e308177d265e63c487f321cd4cdd15fd3264e5
yasminfunk <64017359+yasminfunk@users.noreply.github.com>
2020-05-23 11:51:52 +0200

    create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const {diamond} = require('./diamond');
+const end = process.argv[2]
+//var result = "E".join('\n')
+var alphabet =["E"];
+var result= alphabet.join('\n')
+console.log (diamond(result))
